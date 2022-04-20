7011db639eb13c37b128408a432c4241a7cd0d7b
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-04-20 11:45:16 -0300

    Projeto criado

---
+++ src/test/java/com/aggeo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggeo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
